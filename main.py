151935c4b2740e750245a2022dd9e8c816dfc5f0
srl-git <samrosslaye@gmail.com>
2025-01-03 15:42:41 +0000

    Updated item popularity logic and rewritten generate_orders function

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import numpy as np
+import csv
 
-ENTRIES = 10
+NUM_PRODUCTS = 35
+NUM_ORDERS = 10000
 
-# product sku, item qty, order qty, username, user email
+# product sku, product price, product popularity, product name
+# order ID, items in order, qty items in order, username, user email, address
+# username, user email
+
 
 class Products:
 
@@ -10,70 +15,72 @@ class Products:
         self.label_prefix = label_prefix
         self.entries = entries
         self.skus = [f"{self.label_prefix}{i:03d}" for i in range(1,self.entries + 1)]
-        self.weighting = [round(np.random.rand(),2) for i in range(0,self.entries)]
+        weighting = np.random.rand(self.entries)
+        self.popularity = weighting / weighting.sum()
         self.prices = [int(np.random.choice([18,19,20,21])) for i in range(0,self.entries)]
+        self.price = {sku: price for sku, price in zip(self.skus, self.prices)}
 
 
 def generate_num_items(max_num_items):
     num_items = np.random.randint(1,max_num_items)
     return num_items
 
-def generate_item_qty():
-    range = np.arange(1, 6)
-    weights = np.exp(-0.7 * (range - 1))
-    probabilities = weights / weights.sum()   
-    line_qty = int(np.random.choice(range,p=probabilities))
-    return line_qty
-
-# def generate_order(product_skus,max_num_items):
-#     items_in_order = generate_num_items(max_num_items)
-#     random_product_list = []
-#     random_item_qty_list = []
-
-#     for i in range(items_in_order):
-
-#         random_product = str(np.random.choice(product_skus))
-        
-#         if random_product in random_product_list:
-#             continue
-#         else: 
-#             random_product_list.append(random_product)
-
-#         random_item_qty = generate_item_qty()
-#         random_item_qty_list.append(random_item_qty)
-
-#     order = list(zip(random_product_list,random_item_qty_list))
-#     return order
-
-def generate_order(products,max_num_items):
+def generate_orders(num_orders,products,max_num_items):
     
-    items_in_order = generate_num_items(max_num_items)
-    random_product_list = []
-    random_item_qty_list = []
+    orders = []
 
-    weights = np.array(products.weighting)
-    probabilities = weights / weights.sum()
+    for i in range(num_orders):
 
-    for i in range(items_in_order):
+        order_id = f'{(i + 1):03}'
 
-        random_product = str(np.random.choice(products.skus,p=probabilities))
+        items_in_order = generate_num_items(max_num_items)
+
+        order = {}
+
+        for i in range(items_in_order):
+
+            random_product = str(np.random.choice(products.skus,p=products.popularity))
+            
+            if random_product in order:
+                order[random_product] += 1
+            else:
+                order[random_product] = 1
         
-        if random_product in random_product_list:
-            continue
-        else: 
-            random_product_list.append(random_product)
+        orders.append(order)
 
-        random_item_qty = generate_item_qty()
-        random_item_qty_list.append(random_item_qty)
-
-    order = list(zip(random_product_list,random_item_qty_list))
-    return order
+    return orders
 
 
-products = Products("WARP",ENTRIES)
-all_products = [(products.skus[i],products.prices[i],products.weighting[i]) for i in range(products.entries)]
+warp_products = Products("WARP",NUM_PRODUCTS)
+all_products = [(warp_products.skus[i],warp_products.prices[i],warp_products.popularity[i]) for i in range(warp_products.entries)]
+# print(all_products)
 
-for i in range(30):
 
-    order = generate_order(products,max_num_items=5)
-    print(order)
+# file_path = 'product_data.csv'
+
+# with open(file_path, mode='w', newline='') as file:
+#     writer = csv.writer(file)
+    
+#     writer.writerow(['SKU', 'Price', 'Popularity'])
+    
+#     for row in all_products:
+#         writer.writerow(row)
+
+
+# print(products.skus)
+# print(products.popularity)
+
+
+orders = generate_orders(num_orders=NUM_ORDERS,products=warp_products,max_num_items=5)
+# print(orders)
+
+
+totals = {}
+
+for d in orders:
+    for key, value in d.items():
+        totals[key] = totals.get(key, 0) + value
+
+print(totals)
+
+print(warp_products.price["WARP001"])
