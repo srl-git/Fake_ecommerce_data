b9ca7854853fbf6a6126ea594084aa01c05b2511
srl-git <samrosslaye@gmail.com>
2025-01-04 16:19:07 +0000

    Removed numpy, now using native python random module. Added option to export simplified orders with no customer data apart from customer ID.

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from faker import Faker
-import numpy as np
 import csv
 import random
 
-NUM_PRODUCTS = 60
-NUM_ORDERS = 100
-NUM_USERS = 90
-
+LABEL_PREFIX = 'SDT'
+NUM_PRODUCTS = 100
+NUM_ORDERS = 50000
+NUM_USERS = 45000
 
 class Products:
 
@@ -14,9 +13,9 @@ class Products:
 
         self.label_prefix = label_prefix
         self.entries = entries
-        self.skus = [f"{self.label_prefix}{i:03}" for i in range(1,self.entries + 1)]
-        weighting = np.random.rand(self.entries)
-        self.popularity = weighting / weighting.sum()
+        self.skus = [f'{self.label_prefix}{i:03}' for i in range(1,self.entries + 1)]
+        weighting = [random.random() for _ in range(self.entries)]
+        self.popularity = [w / sum(weighting) for w in weighting]
         self.prices = [random.choice([18,19,20,21]) for i in range(0,self.entries)]
         self.price = {sku: price for sku, price in zip(self.skus, self.prices)}
         
@@ -38,26 +37,25 @@ class Products:
             for row in self.products:
                 writer.writerow(row)
 
-
 class Users:
 
     def __init__(self,num_users):
         
         self.num_users = num_users
-        locales = ["en_GB","en_US","fr_FR","en_CA","de_DE"]
+        locales = ['en_GB','en_US','fr_FR','en_CA','de_DE']
 
         self.users = []
 
         for i in range(num_users):
 
-            user_id = f"{i + 1:05}"
+            user_id = f'{i + 1:05}'
             random_locale = random.choice(locales)
             fake = Faker(random_locale)
             profile = fake.simple_profile()
-            name = profile["name"]
-            address = profile["address"]
+            name = profile['name']
+            address = profile['address']
             country = fake.current_country()
-            email = f"{name.lower().replace(" ","")}@example.com"
+            email = f'{name.lower().replace(' ','').replace('.','')}@example.com'
             user = (user_id, name, address, country, email)
             self.users.append(user)
 
@@ -73,7 +71,6 @@ class Users:
             for row in self.users:
                 writer.writerow(row)            
 
-
 class Orders:
 
     def __init__(self,num_orders,users,products,max_num_items):
@@ -83,11 +80,12 @@ class Orders:
         self.products = products
         self.max_num_items = max_num_items
 
-        self.orders = []
+        self.detailed_orders = []
+        self.simple_orders = []
 
         for i in range(self.num_orders):
 
-            items_in_order = np.random.randint(1,self.max_num_items)
+            items_in_order = random.randint(1,self.max_num_items)
 
             random_customer = random.choice(self.users.users)
             (
@@ -104,8 +102,8 @@ class Orders:
 
             for i in range(items_in_order):
 
-                random_product = str(np.random.choice(products.skus,p=self.products.popularity))
-                
+                random_product = random.choices(products.skus,weights=self.products.popularity)[0]
+
                 if random_product in order_lines:
                     order_lines[random_product] += 1
                 else:
@@ -114,28 +112,36 @@ class Orders:
 
             for sku, qty in order_lines.items():
 
-                order = (order_id, customer_id, customer_name, customer_address, customer_country, customer_email, sku, qty, products.price[sku])
-                self.orders.append(order)
+                detailed_order = (order_id, customer_id, customer_name, customer_address, customer_country, customer_email, sku, qty, products.price[sku])
+                self.detailed_orders.append(detailed_order)
 
-    def to_csv(self,wide=False):
+                simple_order = (order_id, customer_id, sku, qty, products.price[sku])
+                self.simple_orders.append(simple_order)
+
+
+    def to_csv(self,detailed=True):
 
         file_path = 'orders.csv'
 
         with open(file_path, mode='w', newline='') as file:
             writer = csv.writer(file)
             
-            writer.writerow(['Order ID', 'Customer ID', 'Name', 'Address', 'Country', 'Email', 'Item', 'Qty', 'Price'])
+            if detailed:
+                writer.writerow(['Order ID', 'Customer ID', 'Name', 'Address', 'Country', 'Email', 'Item', 'Qty', 'Price'])
+                
+                for row in self.detailed_orders:
+                    writer.writerow(row)
             
-            for row in self.orders:
-                writer.writerow(row)
+            else:
+                writer.writerow(['Order ID', 'Customer ID', 'Item', 'Qty', 'Price'])
+                
+                for row in self.simple_orders:
+                    writer.writerow(row)
 
-
-products = Products(label_prefix="FEEL",entries=NUM_PRODUCTS)
+products = Products(label_prefix=LABEL_PREFIX,entries=NUM_PRODUCTS)
 customers = Users(num_users=NUM_USERS)
 orders = Orders(num_orders=NUM_ORDERS,users=customers,products=products,max_num_items=5)
 
 products.to_csv()
 customers.to_csv()
-orders.to_csv(wide=True)
-
-
+orders.to_csv()
